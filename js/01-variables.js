b79af2f90fe209b09681f3aa4a7ce152ea9a8508
Emilse Arias <emilsearias@gmail.com>
2026-03-02 20:35:01 -0300

    agregue un ejemplo de variables con let y const y usamos el tipo de dato string

---
+++ js/01-variables.js
@@ -0,0 +1,29 @@
+//Esto es un comentario de una sola linea
+
+/* 
+Esto es un comentario
+de multiples
+lineas
+*/
+
+//Declarar una variable (let - const - var)
+
+let nombre = "Dante S.";
+const apellido = "Pereyra";
+
+//mostrar un mensaje en consola
+console.log(nombre);
+console.log("Variable apellido: ", apellido);
+
+//modificar el contenido de una variable
+nombre = "Fatima";
+
+//nunca podemos actualizar el valor de una constante
+//apellido = "Arias" //esto da error
+console.log(nombre);
+
+// Tipos de datos que puedo almacenar en una variable
+//string
+const saludo = `Hola mundo`;
+
+console.log("Ejemplo de string: " + saludo);
