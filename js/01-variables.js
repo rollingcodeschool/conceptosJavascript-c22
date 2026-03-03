ed09b085089be71c23106734dda08e8d0d6a07d6
Emilse Arias <emilsearias@gmail.com>
2026-03-02 21:09:27 -0300

    agregue ejemplos de los distintos tipos de datos primitivos

--- js/01-variables.js
+++ js/01-variables.js
@@ -25,5 +25,28 @@ console.log(nombre);
 // Tipos de datos que puedo almacenar en una variable
 //string
 const saludo = `Hola mundo`;
+//number
+const edad = 25;
+const negativo = -25;
+const precio = 25.5;
+//boolean
+let encendido = true;
+let lavado = false;
+//null
+let alcancia = null;
+//undefined
+let anio;
 
 console.log("Ejemplo de string: " + saludo);
+// console.log("Ejemplo de valores numerico: edad =" + edad + ' negativo=' + negativo + ' precio='+ precio);
+console.log(
+  `Ejemplo de valores numerico: edad =${edad} negativo=${negativo} precio=${precio}`,
+);
+console.log(
+  `Ejemplo de valores booleanos: encendido =${encendido} lavado=${lavado}`,
+);
+console.log(alcancia);
+console.log(anio);
+
+anio = 2026;
+console.log(anio);
