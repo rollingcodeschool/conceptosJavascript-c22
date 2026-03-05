c0fd4b349facd745ac6ca903b89002c1f8eed22e
Emilse Arias <emilsearias@gmail.com>
2026-03-04 21:41:07 -0300

    creamos el archivo para ver la esctructura switch

---
+++ js/03-switch.js
@@ -0,0 +1,8 @@
+/*
+Cree un programa al estilo de un cajero automático con las siguientes opciones:
+1- consultar el saldo
+2- ingresar dinero
+3- extraer dinero 
+*/
+
+const opcion = prompt('Ingresa una opción')
