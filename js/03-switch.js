574c6c78ea34c08a282de1f4437e28a8301f0c10
Emilse Arias <emilsearias@gmail.com>
2026-03-09 20:51:19 -0300

    agregamos mas casos para la opcion de consultar el saldo

--- js/03-switch.js
+++ js/03-switch.js
@@ -22,16 +22,18 @@ let saldo = 10000;
 // }
 
 switch (opcion) {
+  case 'saldo':  
+  case 'SALDO':  
   case "1":
     //aqui agrego todas las lineas necesarias si la opcion es '1'
-    pantalla.innerHTML = `Tu saldo actual es $${saldo}`;
-    break;
+    pantalla.innerHTML += `Tu saldo actual es $${saldo}`;
+   break;
   case "2":
     const deposito = parseFloat(
       prompt("Agrega el dinero que quieres ingresar en tu cuenta"),
     );
     saldo += deposito; //saldo = saldo + deposito
-    pantalla.innerHTML = `Ingresaste $${deposito}, Tu saldo actual es $${saldo}`;
+    pantalla.innerHTML += `Ingresaste $${deposito}, Tu saldo actual es $${saldo}`;
     break;
   case "3":
     const montoExtraer = parseFloat(
@@ -39,7 +41,7 @@ switch (opcion) {
     );
     if (montoExtraer <= saldo) {
       saldo -= montoExtraer; //saldo = saldo - montoExtraer
-      pantalla.innerHTML = `Retiraste $${montoExtraer}, Tu saldo actual es $${saldo}`;
+      pantalla.innerHTML += `Retiraste $${montoExtraer}, Tu saldo actual es $${saldo}`;
     } else {
       alert("Fondos insuficientes");
     }
