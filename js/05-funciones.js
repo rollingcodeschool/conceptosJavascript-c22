064b20830a47e0acb99c5e2e08b62070a27cffc2
Emilse Arias <emilsearias@gmail.com>
2026-03-16 21:14:29 -0300

    agregamos un ejemplo de funcion con valor de retorno

--- js/05-funciones.js
+++ js/05-funciones.js
@@ -1,20 +1,44 @@
-function saludar(){
-    //todas las lineas de codigo que debe hacer esa funcion
-    console.log('Hola mundo 🌎')
+function saludar() {
+  //todas las lineas de codigo que debe hacer esa funcion
+  console.log("Hola mundo 🌎");
+  return "Hola mundo 🌎"
 }
 
-function saludarUsuario(nombreUsuario){
-    const pantalla = document.getElementById('pantalla')
-    pantalla.innerHTML += `<p>Hola ${nombreUsuario}</p>`
+function saludarUsuario(nombreUsuario, edad = 20) {
+  const pantalla = document.getElementById("pantalla");
+  pantalla.innerHTML += `<p>Hola ${nombreUsuario}, tienes ${edad} años</p>`;
+  console.log(`Hola ${nombreUsuario}, tienes ${edad} años`);
 }
+
+// funcion con valor de retorno
+function calcularDescuento(precio, descuento) {
+//   const total = 
+  return precio - (precio * descuento) / 100;
+}
+
 //invocar a una funcion
-saludar()
+saludar();
 
-console.log(1,2,3)
-console.log(2026)
+console.log(1, 2, 3);
+console.log(2026);
 
-saludar()
-saludar()
+saludar();
+saludar();
 
-const usuario = prompt('Ingresa tu nombre')
-saludarUsuario(usuario)
+// do{
+//     const usuario = prompt('Ingresa tu nombre')
+//     const edad = prompt('Ingresa tu edad')
+//     saludarUsuario(usuario, edad)
+//     saludarUsuario('Maria Belen')
+
+// }while(confirm('¿Queres realizar otra operacion?'))
+
+const precio = parseFloat(prompt("Ingresa el precio del producto"));
+const descuento = parseInt(prompt("Ingresa el porcetanje de descuento"));
+
+const total = calcularDescuento(precio, descuento);
+
+const pantalla = document.getElementById("pantalla");
+pantalla.innerHTML += `<p>El precio del producto es $${precio}, el descuento aplicado es de ${descuento}%, el total a pagar es $${total}</p>`;
+
+pantalla.innerHTML += `<p>El precio del producto es $3000, el descuento aplicado es de 50%, el total a pagar es $${calcularDescuento(3000,50)}</p>`;
