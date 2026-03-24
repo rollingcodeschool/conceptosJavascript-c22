d050f023727370c0af1f87a621d25ac829a90fb7
Emilse Arias <emilsearias@gmail.com>
2026-03-23 21:34:35 -0300

    agregamos un ejemplo con las propiedades de un objeto pelicula

---
+++ js/08-objetos.js
@@ -0,0 +1,20 @@
+// objeto: celular
+//propiedades: marca, memoria, procesador, precio, color
+//metodos: tomarFoto(), enviarMensaje()
+
+//objeto: cancion
+//propiedades: genero, duracion, cantante, anio, titulo
+//metodos: reproducir(), pausar(), modificarVelocidad(velocidad)
+
+//notacion literal
+const pelicula = {
+    //propiedades
+    titulo: 'toys story',
+    genero: 'animacion',
+    duracion: '02:30',
+    actores: ['Tom hanks', 'actor 2', 'actor 3'],
+    anio: 1995
+    //metodos
+}
+
+console.log(pelicula)
