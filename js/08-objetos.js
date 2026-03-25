dbbe81b9b6b04e81e8c3d308eb959f6c1c2709a4
Emilse Arias <emilsearias@gmail.com>
2026-03-25 20:46:08 -0300

    agregamos un ejemplo de como modificar y eliminar una propiedad

--- js/08-objetos.js
+++ js/08-objetos.js
@@ -14,10 +14,10 @@ const pelicula = {
   duracion: "02:30",
   actores: ["Tom hanks", "actor 2", "actor 3"],
   anio: 1995,
-//   estudio: {
-//     nombre: 'pixar',
-//     anio: 1994
-//   },
+  //   estudio: {
+  //     nombre: 'pixar',
+  //     anio: 1994
+  //   },
   //metodos
   reproducir: function () {
     console.log("Comenzo la reproducción de la pelicula");
@@ -39,29 +39,43 @@ const pelicula = {
 console.log(pelicula);
 
 //mostramos en pantalla un objeto
-const pantalla = document.getElementById('pantalla')
-pantalla.innerHTML = `<p>${pelicula}</p>` 
-pantalla.innerHTML += `<p>Nombre de pelicula: ${pelicula.titulo}</p>` 
-pantalla.innerHTML += `<p>Genero: ${ pelicula['genero'] }</p>` 
-// pantalla.innerHTML += `<p>Estudio: ${ pelicula.estudio.nombre }</p>` 
-// pantalla.innerHTML += `<p>Estudio: ${ pelicula.estudio.anio }</p>` 
-// pantalla.innerHTML += `<p>Estudio: ${ pelicula.estudio }</p>` 
-pantalla.innerHTML += `<p>Genero: ${ pelicula['Genero'] }</p>` // cuando la propiedad esta mal escrita vemos un undefined
+const pantalla = document.getElementById("pantalla");
+pantalla.innerHTML = `<p>${pelicula}</p>`;
+pantalla.innerHTML += `<p>Nombre de pelicula: ${pelicula.titulo}</p>`;
+pantalla.innerHTML += `<p>Genero: ${pelicula["genero"]}</p>`;
+// pantalla.innerHTML += `<p>Estudio: ${ pelicula.estudio.nombre }</p>`
+// pantalla.innerHTML += `<p>Estudio: ${ pelicula.estudio.anio }</p>`
+// pantalla.innerHTML += `<p>Estudio: ${ pelicula.estudio }</p>`
+pantalla.innerHTML += `<p>Genero: ${pelicula["Genero"]}</p>`; // cuando la propiedad esta mal escrita vemos un undefined
 
 // mostrar todas las propieades de un objeto for ... in
-let textoPantalla = `<h3>Propiedades del objeto Pelicula</h3>`
-for(const propiedad in pelicula){
-    if(typeof pelicula[propiedad] !== 'function'){
-        const valor = pelicula[propiedad]
-        textoPantalla += `<p>${propiedad}: ${valor}</p>`
-    }
+let textoPantalla = `<h3>Propiedades del objeto Pelicula</h3>`;
+for (const propiedad in pelicula) {
+  if (typeof pelicula[propiedad] !== "function") {
+    const valor = pelicula[propiedad];
+    textoPantalla += `<p>${propiedad}: ${valor}</p>`;
+  }
 }
 
-pantalla.innerHTML += textoPantalla
-console.log(typeof 'hola mundo')
-console.log(typeof 20)
-console.log(typeof pelicula)
-console.log(typeof pelicula.anio)
-console.log(typeof pelicula.titulo)
-console.log(typeof pelicula.reproducir)
-console.log(typeof pelicula.stop)
+pantalla.innerHTML += textoPantalla;
+console.log(typeof "hola mundo");
+console.log(typeof 20);
+console.log(typeof pelicula);
+console.log(typeof pelicula.anio);
+console.log(typeof pelicula.titulo);
+console.log(typeof pelicula.reproducir);
+console.log(typeof pelicula.stop);
+pantalla.innerHTML += `<hr>`;
+//Como agregar una propiedad nueva al objeto
+pantalla.innerHTML += `<p>Publicado : ${pelicula.publicado}</p>`;
+pelicula.publicado = true;
+pantalla.innerHTML += `<p>Publicado : ${pelicula.publicado}</p>`;
+
+//Como modificar una propiedad del objeto
+pelicula.anio = 1996;
+pantalla.innerHTML += `<p>Año : ${pelicula.anio}</p>`;
+
+//como eliminar una propiedad del objeto
+delete pelicula.publicado;
+pantalla.innerHTML += `<p>Publicado : ${pelicula.publicado ? "Si" : "No existe"}</p>`;
+console.log(pelicula);
